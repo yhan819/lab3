209882858e3da8c5eecfbe97c32cabc8597e3031
Yoon-Suk (John) Han <yhan819@stanford.edu>
2014-01-22 22:11:54 +0000

    javascript lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,38 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("It's Connected!!!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(updateProject);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
+
+function updateProject(e) {
+	var project_val = $("#project").val();
+	var width = $("#width").val();
+	var description = $("#description").val();
+
+	$(project_val).animate({
+		width: width,
+	});
+	$(project_val + " .project-description").text(description);
 }
